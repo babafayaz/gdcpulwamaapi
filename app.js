541cadeb9d9099f8551ffc7c5a078442fe328db1
babafayaz <68767235+babafayaz@users.noreply.github.com>
2020-08-12 19:45:56 +0530

    make it better

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = process.env.port || 5000;
-const notifications =require('./routes/notifications');
-app.use("/notifications", notifications);
+
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
@@ -13,7 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.redirect('https://gdcpulwama.edu.in');
 })
-
+app.get('/test', (req, res) => {
+  res.redirect('https://test.edu.in');
+})
 app.listen(port, () => {
   console.log(`GDC app listening at http://localhost:${port}`)
 })
