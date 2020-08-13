396bd82f4f1dc2d6f9dead968ab674cebcafc4b8
babafayaz <68767235+babafayaz@users.noreply.github.com>
2020-08-13 12:17:23 +0530

    make it better

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,10 +1,11 @@
 const express = require("express");
 let router = express.Router();
 var db = require('../db/db');
-
+let path = require('path');
+const { request } = require("http");
 router.route('/notification')
   .get(function (req, res) {
-    db.query('SELECT * FROM notification ORDER BY id desc', (err, result) => {
+    db.query('SELECT * FROM notification ORDER BY pubdate desc', (err, result) => {
         if (err){
             console.log("error: ", err);
             
@@ -34,6 +35,30 @@ router.route('/notification')
     });
     
   
+})
+
+router.route('/download/:id')
+  .get(function (req, res) {
+    let id=[req.params.id]
+    db.query('SELECT filename FROM notification WHERE id = ? ',id ,(err, result) => {
+      if (err){
+          console.log("error: ", err);
+          
+          return;
+        }
+
+        //res.status(200).send(result);
+       
+        res.download(path.join(__dirname, '../attachments/123.pdf'), (err)=>{
+        console.log(err);
+        console.log('Your file has been downloaded!')
+        });
+       
+    });
+   // res.status(200).send({fn}),
+
+
 });
+ 
 
 module.exports = router;
