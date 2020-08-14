19464d0828bf16399bd7c4113abc8e780853511a
babafayaz <68767235+babafayaz@users.noreply.github.com>
2020-08-14 11:41:25 +0530

    make it better

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var cors = require('cors');
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const notifications =require('./routes/notifications');
+const login = require('./routes/login')
 app.use(cors());
-
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/notifications", notifications);
+app.use("/login", login)
+
 app.get('/', (req, res) => {
   res.redirect('https://gdcpulwama.edu.in');
 })
--- routes/notifications.js
+++ routes/notifications.js
@@ -3,62 +3,61 @@ let router = express.Router();
 var db = require('../db/db');
 let path = require('path');
 const { request } = require("http");
-router.route('/notification')
-  .get(function (req, res) {
-    db.query('SELECT * FROM notification ORDER BY pubdate desc', (err, result) => {
-        if (err){
-            console.log("error: ", err);
-            
-            return;
-          }
-       
-        res.send(result);
-      });
 
+
+//requestes on /notification/:category
+router.route('/notification/:category')
+  .get(function (req, res) {                       //notification get request depending on category
+      const cat=[req.params.category]
+      db.query(`SELECT * FROM notification WHERE category= ? && isactive= 1 ORDER BY pubdate desc`,cat, (err, result) => {
+          if (err){
+              res.status(500).send(err);
+              return;
+            }
+          res.send(result);
+        });
   })
-  .post(function (req, res) {
-   
-    let data = {
-      title : req.body.title,
-       filename : req.body.filename, 
-       link: req.body.link,
-       pubdate: req.body.pubdate,
-       category: req.body.category, 
-       discription: req.body.discription,
-       isactive: req.body.isactive, 
-       notificationtype: req.body.notificationtype
-      };
+  //notification post
+  .post(function (req, res) {  
+      let data = {
+                    title : req.body.title,
+                    filename : req.body.filename, 
+                    link: req.body.link,
+                    pubdate: req.body.pubdate,
+                    category: req.body.category, 
+                    discription: req.body.discription,
+                    isactive: req.body.isactive, 
+                    notificationtype: req.body.notificationtype
+                  };
     let sql = "INSERT INTO notification SET ?";
      db.query(sql, data,(err, results) => {
-      if(err) throw err;
-      res.send("one recored addedd")
-    });
-    
-  
+      if (err){
+                res.status(500).send(err);
+                return;
+              }
+      res.json({"msg": "Notification added successfully"})
+      }); 
 })
-
+//api for downloading the pdf files /notifications on id passed as params
 router.route('/download/:id')
   .get(function (req, res) {
-    let id=[req.params.id]
-    db.query('SELECT filename FROM notification WHERE id = ? ',id ,(err, result) => {
-      if (err){
-          console.log("error: ", err);
-          
-          return;
-        }
-
-        //res.status(200).send(result);
-       
-        res.download(path.join(__dirname, '../attachments/' + id +".pdf"), (err)=>{
-        console.log(err);
-        console.log('Your file has been downloaded!')
-        });
-       
-    });
-   // res.status(200).send({fn}),
-
-
+            let id=[req.params.id]
+            db.query('SELECT filename FROM notification WHERE id = ? ',id ,(err, result) => {
+              if (err){
+                res.status(500).send(err);
+                return;
+                }
+                //res.status(200).send(result);       
+                res.download(path.join(__dirname, '../attachments/' + id +".pdf"), (err)=>{
+                  if (err){
+                            res.status(500).send(err);
+                            return;
+                          }
+                });              
+            });
+          // res.status(200).send({fn}),
 });
- 
+
 
 module.exports = router;
+
