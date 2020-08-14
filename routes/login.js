649d985e318314181255f43cef6cc7da077e5523
babafayaz <68767235+babafayaz@users.noreply.github.com>
2020-08-14 16:30:48 +0530

    make it better

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,9 @@ const express = require("express");
 let router = express.Router();
 var db = require('../db/db');
 const { Router } = require("express");
-
+const jwt = require("jsonwebtoken");
+const jwtKey = "my_secret_key";
+const jwtExpirySeconds = 300;
 
 //login api
 router.route('/')
@@ -16,19 +18,28 @@ let sql = "SELECT * FROM user_tbl WHERE username = '"+ USER +"' && password = '"
  db.query(sql,(err, results) => {
   if (err){
             res.status(500).send(err);
-            res.json({"msg": "iNALID lOGIN"})
             return;
           }
     else
     {
         if(results.length){
+                
+                const token = jwt.sign({USER}, jwtKey, {
+                algorithm: "HS256",
+                expiresIn: jwtExpirySeconds,
+            })
+           
+            // set the cookie as the token string, with a similar max age as the token
+            // here, the max age is in milliseconds, so we multiply by 1000
+            res.cookie("token", token, { maxAge: jwtExpirySeconds * 1000 })
             res.status(200);
-            res.json({"msg": "Valid user name and password "})
-            res.redirect('/');
+            res.json({"msg": "Token set"})
+            res.end()
          }
          else
          {
-             res.json({"msg": "invalid Credientials"})
+            res.status(401); 
+            res.json({"msg": "invalid Credientials"})
          }
     }
  
