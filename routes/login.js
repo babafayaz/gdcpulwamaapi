533c72beec15881191904f15e3887c6c5c1807c4
babafayaz <68767235+babafayaz@users.noreply.github.com>
2020-08-14 11:42:10 +0530

    make it better

---
+++ routes/login.js
@@ -0,0 +1,37 @@
+const express = require("express");
+let router = express.Router();
+var db = require('../db/db');
+const { Router } = require("express");
+
+
+//login api
+router.route('/')
+.post(function (req, res) {  
+  
+            const USER= [req.body.username];
+            const PWD = [req.body.pwd];
+            
+            
+let sql = "SELECT * FROM user_tbl WHERE username = '"+ USER +"' && password = '"+PWD+"'";
+ db.query(sql,(err, results) => {
+  if (err){
+            res.status(500).send(err);
+            res.json({"msg": "iNALID lOGIN"})
+            return;
+          }
+    else
+    {
+        if(results.length){
+            res.json({"msg": "Valid user name and password "})
+           // res.redirect('/home/dashboard');
+         }
+         else
+         {
+             res.json({"msg": "invalid Credientials"})
+         }
+    }
+ 
+  }); 
+})
+
+module.exports = router;
