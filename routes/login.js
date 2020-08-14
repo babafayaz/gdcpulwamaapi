2f601526b1332d5b95caf9a1fbb43481aece1eaa
babafayaz <68767235+babafayaz@users.noreply.github.com>
2020-08-14 17:22:48 +0530

    make it better

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,6 @@
 const express = require("express");
 let router = express.Router();
 var db = require('../db/db');
-const { Router } = require("express");
 const jwt = require("jsonwebtoken");
 const jwtKey = "my_secret_key";
 const jwtExpirySeconds = 300;
@@ -24,14 +23,7 @@ let sql = "SELECT * FROM user_tbl WHERE username = '"+ USER +"' && password = '"
     {
         if(results.length){
                 
-                const token = jwt.sign({USER}, jwtKey, {
-                algorithm: "HS256",
-                expiresIn: jwtExpirySeconds,
-            });
-           
-            // set the cookie as the token string, with a similar max age as the token
-            // here, the max age is in milliseconds, so we multiply by 1000
-            res.cookie("token", token, { maxAge: jwtExpirySeconds * 1000 })
+               
             res.status(200);
             res.json({"msg": "Token set"})
             res.end()
